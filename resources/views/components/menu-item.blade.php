a6813dddd73aabb14214f52e04d2fed10c922650
itotchiya <toozmust@gmail.com>
2025-04-04 11:07:39 +0000

    latest change idont know

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -26,11 +26,10 @@
         <a class="nav-item text-lightest f-15 sidebar-text-color accordionItemHeading {{ $active == 1 ? 'active' : '' }}"
            title="{{ $text }}">
             @isset($iconPath)
-            <x-lucide-bike class="icon-small"/>
-            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     class="bi bi-{{ $icon }}" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="nav-item-icon" viewBox="0 0 16 16">
                     {!! $iconPath !!}
-                </svg> -->
+            </svg>
             @endisset
             <span class="pl-3">{{ $text }}</span>
 
